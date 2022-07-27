theorchidproject/packages_apps_GameSpace

GameSpace: Port in-game danmaku feature
* Adapt from https://github.com/Nameless-AOSP/packages_apps_GamingMode.
* Add back tile for notification mode switch.

Co-authored-by: jhonboy121 <alfredmathew05@gmail.com>
Co-authored-by: cjh1249131356 <cjh1249131356@gmail.com>
Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>

app/src/main/java/org/nameless/gamespace/data/AppSettings.kt
@@ -58,7 +58,7 @@ class AppSettings @Inject constructor(private val context: Context) {
         set(value) = db.edit().putString(KEY_CALLS_MODE, value.toString()).apply()
 
     var notificationsMode: Int
-        get() = db.getString(KEY_NOTIFICAITONS_MODE, "2").toInt()
+        get() = db.getString(KEY_NOTIFICAITONS_MODE, "3").toInt()
         set(value) = db.edit().putString(KEY_NOTIFICAITONS_MODE, value.toString()).apply()
 
     var ringerMode: Int

app/src/main/java/org/nameless/gamespace/data/GameSession.kt
@@ -72,13 +72,14 @@ class GameSession @Inject constructor(
         if (appSettings.noAdbEnabled) {
             systemSettings.adbEnabled = false
         }
-        if (appSettings.notificationsMode == 0) {
+        if (appSettings.notificationsMode == 0 || appSettings.notificationsMode == 3) {
             systemSettings.headsUp = false
             systemSettings.reTicker = false
         } else if (appSettings.notificationsMode == 1) {
             systemSettings.headsUp = true
             systemSettings.reTicker = false
         } else {
+            systemSettings.headsUp = true
             systemSettings.reTicker = true
         }
         if (appSettings.ringerMode != 3) {
@@ -97,9 +98,7 @@ class GameSession @Inject constructor(
         if (appSettings.noAdbEnabled) {
             orig.adbEnabled?.let { systemSettings.adbEnabled = it }
         }
-        if (appSettings.notificationsMode != 2) {
-            orig.headsUp?.let { systemSettings.headsUp = it }
-        }
+        orig.headsUp?.let { systemSettings.headsUp = it }
         orig.reTicker?.let { systemSettings.reTicker = it }
         if (appSettings.ringerMode != 3) {
             audioManager.ringerModeInternal = orig.ringerMode

app/src/main/java/org/nameless/gamespace/widget/tiles/NotificationTile.kt
@@ -1,0 +1,75 @@
+/*
+ * Copyright (C) 2021 Chaldeaprjkt
+ * Copyright (C) 2022 Nameless-AOSP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.nameless.gamespace.widget.tiles
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import org.nameless.gamespace.R
+
+class NotificationTile @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null
+) : BaseTile(context, attrs) {
+
+    private var activeMode = DANMAKU_MODE
+        set(value) {
+            field = value
+            appSettings.notificationsMode = value
+            when (value) {
+                NO_NOTIFICATION -> {
+                    systemSettings.headsUp = false
+                    systemSettings.reTicker = false
+                    summary?.text = context.getString(R.string.notifications_hide)
+                }
+                HEADS_UP_MODE -> {
+                    systemSettings.headsUp = true
+                    systemSettings.reTicker = false
+                    summary?.text = context.getString(R.string.notifications_headsup)
+                }
+                RETICKER_MODE -> {
+                    systemSettings.headsUp = true
+                    systemSettings.reTicker = true
+                    summary?.text = context.getString(R.string.notifications_reticker)
+                }
+                DANMAKU_MODE -> {
+                    systemSettings.headsUp = false
+                    systemSettings.reTicker = false
+                    summary?.text = context.getString(R.string.notifications_danmaku)
+                }
+            }
+            isSelected = value != NO_NOTIFICATION
+        }
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+        title?.text = context.getString(R.string.notifications_title)
+        activeMode = appSettings.notificationsMode
+        icon?.setImageResource(R.drawable.ic_action_heads_up)
+    }
+
+    override fun onClick(v: View?) {
+        super.onClick(v)
+        activeMode = if (activeMode == DANMAKU_MODE) NO_NOTIFICATION else activeMode + 1
+    }
+
+    companion object {
+        private const val NO_NOTIFICATION = 0
+        private const val HEADS_UP_MODE = 1
+        private const val RETICKER_MODE = 2
+        private const val DANMAKU_MODE = 3
+    }
+}

bananadroid/android_packages_apps_GameSpace

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

app/src/main/java/io/chaldeaprjkt/gamespace/data/AppSettings.kt
@@ -54,7 +54,7 @@ class AppSettings @Inject constructor(private val context: Context) {
         set(value) = db.edit().putBoolean(KEY_STAY_AWAKE, value).apply()
 
     var notificationMode: Int
-        get() = db.getString(KEY_NOTIFICATION_MODE, "2").toInt()
+        get() = db.getString(KEY_NOTIFICATION_MODE, "3").toInt()
         set(value) = db.edit().putString(KEY_NOTIFICATION_MODE, value.toString()).apply()
 
     var ringerMode: Int

app/src/main/java/io/chaldeaprjkt/gamespace/data/GameSession.kt
@@ -72,13 +72,14 @@ class GameSession @Inject constructor(
         if (appSettings.noAdbEnabled) {
             systemSettings.adbEnabled = false
         }
-        if (appSettings.notificationMode == 0) {
+        if (appSettings.notificationMode == 0 || appSettings.notificationMode == 3) {
             systemSettings.headsUp = false
             systemSettings.reTicker = false
         } else if (appSettings.notificationMode == 1) {
             systemSettings.headsUp = true
             systemSettings.reTicker = false
         } else {
+            systemSettings.headsUp = true
             systemSettings.reTicker = true
         }
         if (appSettings.ringerMode != 3) {
@@ -97,9 +98,7 @@ class GameSession @Inject constructor(
         if (appSettings.noAdbEnabled) {
             orig.adbEnabled?.let { systemSettings.adbEnabled = it }
         }
-        if (appSettings.notificationMode != 2) {
-            orig.headsUp?.let { systemSettings.headsUp = it }
-        }
+        orig.headsUp?.let { systemSettings.headsUp = it }
         orig.reTicker?.let { systemSettings.reTicker = it }
         if (appSettings.ringerMode != 3) {
             audioManager.ringerModeInternal = orig.ringerMode

app/src/main/java/io/chaldeaprjkt/gamespace/widget/tiles/NotificationTile.kt
@@ -1,0 +1,75 @@
+/*
+ * Copyright (C) 2021 Chaldeaprjkt
+ * Copyright (C) 2022 Nameless-AOSP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.chaldeaprjkt.gamespace.widget.tiles
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import io.chaldeaprjkt.gamespace.R
+
+class NotificationTile @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null
+) : BaseTile(context, attrs) {
+
+    private var activeMode = DANMAKU_MODE
+        set(value) {
+            field = value
+            appSettings.notificationMode = value
+            when (value) {
+                NO_NOTIFICATION -> {
+                    systemSettings.headsUp = false
+                    systemSettings.reTicker = false
+                    summary?.text = context.getString(R.string.notification_hide)
+                }
+                HEADS_UP_MODE -> {
+                    systemSettings.headsUp = true
+                    systemSettings.reTicker = false
+                    summary?.text = context.getString(R.string.notification_headsup)
+                }
+                RETICKER_MODE -> {
+                    systemSettings.headsUp = true
+                    systemSettings.reTicker = true
+                    summary?.text = context.getString(R.string.notification_reticker)
+                }
+                DANMAKU_MODE -> {
+                    systemSettings.headsUp = false
+                    systemSettings.reTicker = false
+                    summary?.text = context.getString(R.string.notification_danmaku)
+                }
+            }
+            isSelected = value != NO_NOTIFICATION
+        }
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+        title?.text = context.getString(R.string.notification_mode_title)
+        activeMode = appSettings.notificationMode
+        icon?.setImageResource(R.drawable.ic_action_heads_up)
+    }
+
+    override fun onClick(v: View?) {
+        super.onClick(v)
+        activeMode = if (activeMode == DANMAKU_MODE) NO_NOTIFICATION else activeMode + 1
+    }
+
+    companion object {
+        private const val NO_NOTIFICATION = 0
+        private const val HEADS_UP_MODE = 1
+        private const val RETICKER_MODE = 2
+        private const val DANMAKU_MODE = 3
+    }
+}